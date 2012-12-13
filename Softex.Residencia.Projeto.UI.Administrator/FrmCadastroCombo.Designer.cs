ac43ffd186554b6cc57d4bd0958dea9739258858
Marc SJ <marcwebbie@gmail.com>
2012-12-13 04:31:45 +0100

    Modificação da interface do administrador e adição de dos forms de cadastros (Ingrediente, Produto, Categoria, Combo)

---
+++ Softex.Residencia.Projeto.UI.Administrator/FrmCadastroCombo.Designer.cs
@@ -0,0 +1,43 @@
+﻿namespace Softex.Residencia.Projeto.UI.Administrator {
+    partial class FrmCadastroCombo {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmCadastroCombo));
+            this.SuspendLayout();
+            // 
+            // FrmCadastroCombo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmCadastroCombo";
+            this.Text = "Cadastro de combo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
