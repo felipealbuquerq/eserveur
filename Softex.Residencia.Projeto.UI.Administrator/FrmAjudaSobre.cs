45b2defa60dd67583dd1b7f51104023a8a888706
Marc SJ <marcwebbie@gmail.com>
2012-12-21 01:28:37 +0100

    Modificacao da interface adicao de tooltips e positionamento dos controls

---
+++ Softex.Residencia.Projeto.UI.Administrator/FrmAjudaSobre.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Softex.Residencia.Projeto.UI.Administrator {
+    public partial class FrmAjudaSobre : Softex.Residencia.Projeto.UI.Administrator.FrmBDCategoria {
+        public FrmAjudaSobre() {
+            InitializeComponent();
+        }
+    }
+}
